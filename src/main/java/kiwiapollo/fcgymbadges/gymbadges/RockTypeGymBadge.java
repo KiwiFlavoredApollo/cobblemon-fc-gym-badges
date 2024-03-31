ad12e67af68f0dac8e783226da50d8efd157a7aa
KiwiFlavoredApollo <fractalbarista@gmail.com>
2024-03-31 11:56:38 +0900

    added rock type gym badge

---
+++ src/main/java/kiwiapollo/fcgymbadges/gymbadges/RockTypeGymBadge.java
@@ -0,0 +1,8 @@
+package kiwiapollo.fcgymbadges.gymbadges;
+
+public class RockTypeGymBadge extends GymBadge {
+    @Override
+    protected void defineItemName() {
+        itemName = "rock_type";
+    }
+}

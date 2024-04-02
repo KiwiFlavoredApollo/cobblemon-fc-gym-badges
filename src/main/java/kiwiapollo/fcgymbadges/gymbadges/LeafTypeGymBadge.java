b85112aca75c64390a0deb51a6a243fdcc7caac6
KiwiFlavoredApollo <fractalbarista@gmail.com>
2024-04-02 15:23:46 +0900

    renamed from leaf_type to leaf_badge

--- src/main/java/kiwiapollo/fcgymbadges/gymbadges/LeafTypeGymBadge.java
+++ src/main/java/kiwiapollo/fcgymbadges/gymbadges/LeafTypeGymBadge.java
@@ -2,6 +2,6 @@ package kiwiapollo.fcgymbadges.gymbadges;
 
 public class LeafTypeGymBadge extends GymBadge {
     public LeafTypeGymBadge() {
-        super("leaf_type");
+        super("leaf_badge");
     }
 }

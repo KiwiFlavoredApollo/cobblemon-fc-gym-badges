a9cce743922ec4edbd46ac5115803e63e0fab712
KiwiFlavoredApollo <fractalbarista@gmail.com>
2024-04-02 14:58:49 +0900

    added line breaks for better readability

--- src/main/java/kiwiapollo/fcgymbadges/gymbadges/GymBadge.java
+++ src/main/java/kiwiapollo/fcgymbadges/gymbadges/GymBadge.java
@@ -8,13 +8,13 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public abstract class GymBadge {
-
     private final Item item;
     private final Identifier identifier;
 
     public GymBadge(String itemName) {
         this.item = new Item(new FabricItemSettings());
         this.identifier = new Identifier(FractalCoffeeGymBadges.NAMESPACE, itemName);
+
         registerItem();
     }
 

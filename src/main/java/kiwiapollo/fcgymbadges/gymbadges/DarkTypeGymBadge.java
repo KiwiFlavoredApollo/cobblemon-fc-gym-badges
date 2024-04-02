20c345aafb3e8c930384a27e3903f48f5fa5df8a
KiwiFlavoredApollo <fractalbarista@gmail.com>
2024-04-02 15:23:09 +0900

    renamed from dark_type to dark_badge

--- src/main/java/kiwiapollo/fcgymbadges/gymbadges/DarkTypeGymBadge.java
+++ src/main/java/kiwiapollo/fcgymbadges/gymbadges/DarkTypeGymBadge.java
@@ -2,6 +2,6 @@ package kiwiapollo.fcgymbadges.gymbadges;
 
 public class DarkTypeGymBadge extends GymBadge {
     public DarkTypeGymBadge() {
-        super("dark_type");
+        super("dark_badge");
     }
 }

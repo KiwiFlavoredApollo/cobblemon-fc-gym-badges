908187c43796a4e70940fa71710d68198d037b94
KiwiFlavoredApollo <fractalbarista@gmail.com>
2024-04-05 19:29:53 +0900

    edited PlayerGymBadgeExistException constructor argument

--- src/main/java/kiwiapollo/fcgymbadges/exceptions/PlayerGymBadgeExistException.java
+++ src/main/java/kiwiapollo/fcgymbadges/exceptions/PlayerGymBadgeExistException.java
@@ -1,7 +1,5 @@
 package kiwiapollo.fcgymbadges.exceptions;
 
 public class PlayerGymBadgeExistException extends Exception {
-    public PlayerGymBadgeExistException(String message) {
-        super(message);
-    }
+
 }

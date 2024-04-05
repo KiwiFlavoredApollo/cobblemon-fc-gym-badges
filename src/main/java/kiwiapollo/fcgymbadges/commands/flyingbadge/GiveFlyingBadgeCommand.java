6314d9c5bb803aa5315bd0390669ec42f029a4f2
KiwiFlavoredApollo <fractalbarista@gmail.com>
2024-04-05 19:23:56 +0900

    added give/clear command for flying badge

---
+++ src/main/java/kiwiapollo/fcgymbadges/commands/flyingbadge/GiveFlyingBadgeCommand.java
@@ -0,0 +1,36 @@
+package kiwiapollo.fcgymbadges.commands.flyingbadge;
+
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import kiwiapollo.fcgymbadges.commands.GiveGymBadgeCommand;
+import kiwiapollo.fcgymbadges.exceptions.PlayerGymBadgeExistException;
+import kiwiapollo.fcgymbadges.storage.GymBadgeCase;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
+
+public class GiveFlyingBadgeCommand extends GiveGymBadgeCommand {
+    public GiveFlyingBadgeCommand() {
+        super("Flying Badge");
+    }
+
+    @Override
+    protected void giveGymBadgeToPlayer(CommandContext<ServerCommandSource> context)
+            throws CommandSyntaxException {
+        ServerPlayerEntity player = getPlayerArgument(context);
+        GymBadgeCase gymBadgeCase = new GymBadgeCase(player);
+        gymBadgeCase.addFlyingBadge();
+        gymBadgeCase.save();
+    }
+
+    @Override
+    protected void assertNotExistPlayerBadge(CommandContext<ServerCommandSource> context)
+            throws PlayerGymBadgeExistException, CommandSyntaxException {
+
+        ServerPlayerEntity player = getPlayerArgument(context);
+        GymBadgeCase gymBadgeCase = new GymBadgeCase(player);
+
+        if(gymBadgeCase.isExistFlyingBadge()) {
+            throw(new PlayerGymBadgeExistException());
+        }
+    }
+}

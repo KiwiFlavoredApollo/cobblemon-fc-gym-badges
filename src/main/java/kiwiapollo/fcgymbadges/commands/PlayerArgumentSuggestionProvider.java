d60d765da1c7e190a91922cbc2522b4e44169c3f
KiwiFlavoredApollo <fractalbarista@gmail.com>
2024-06-20 23:30:20 +0900

    moved to fcgymbadges/commands

--- src/main/java/kiwiapollo/fcgymbadges/commands/PlayerArgumentSuggestionProvider.java
+++ src/main/java/kiwiapollo/fcgymbadges/commands/PlayerArgumentSuggestionProvider.java
@@ -1,4 +1,4 @@
-package kiwiapollo.fcgymbadges.commands.suggestion;
+package kiwiapollo.fcgymbadges.commands;
 
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;

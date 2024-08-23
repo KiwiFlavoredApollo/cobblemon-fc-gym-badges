58aadc74a9ade769a421fd3c1c2096ab14756c21
KiwiFlavoredApollo <fractalbarista@gmail.com>
2024-08-23 18:20:44 +0900

    edited GymBadge class to only act as a data structure

--- src/main/java/kiwiapollo/fcgymbadges/gymbadges/GymBadge.java
+++ src/main/java/kiwiapollo/fcgymbadges/gymbadges/GymBadge.java
@@ -3,28 +3,16 @@ package kiwiapollo.fcgymbadges.gymbadges;
 import kiwiapollo.fcgymbadges.FractalCoffeeGymBadges;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
+import java.util.List;
+
 public class GymBadge {
-    private final Item item;
-    private final Identifier identifier;
+    public final Item item;
+    public final Identifier identifier;
 
-    public GymBadge(String itemName) {
-        this.item = new Item(new FabricItemSettings());
-        this.identifier = new Identifier(FractalCoffeeGymBadges.NAMESPACE, itemName);
-    }
-
-    public Item getItem() {
-        return item;
-    }
-
-    public String getName() {
-        return identifier.getPath();
-    }
-
-    public void register() {
-        Registry.register(Registries.ITEM, identifier, item);
+    public GymBadge(String path) {
+        this.item = new Item(new FabricItemSettings().maxCount(1));
+        this.identifier = Identifier.of(FractalCoffeeGymBadges.NAMESPACE, path);
     }
 }

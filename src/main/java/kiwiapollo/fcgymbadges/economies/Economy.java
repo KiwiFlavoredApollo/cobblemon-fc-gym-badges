504891da3c8f72ee478d23e4a239ba892f2c7c9a
KiwiFlavoredApollo <fractalbarista@gmail.com>
2024-08-24 16:47:01 +0900

    removed unused imports and redundant public modifier

--- src/main/java/kiwiapollo/fcgymbadges/economies/Economy.java
+++ src/main/java/kiwiapollo/fcgymbadges/economies/Economy.java
@@ -1,11 +1,10 @@
 package kiwiapollo.fcgymbadges.economies;
 
-import kiwiapollo.fcgymbadges.exceptions.NotEnoughBalanceException;
 import net.minecraft.server.network.ServerPlayerEntity;
 
 public interface Economy {
-    public double getBalance(ServerPlayerEntity player);
-    public void addBalance(ServerPlayerEntity player, double amount);
-    public void removeBalance(ServerPlayerEntity player, double amount);
-    public boolean isExistEnoughBalance(ServerPlayerEntity player, double amount);
+    double getBalance(ServerPlayerEntity player);
+    void addBalance(ServerPlayerEntity player, double amount);
+    void removeBalance(ServerPlayerEntity player, double amount);
+    boolean isExistEnoughBalance(ServerPlayerEntity player, double amount);
 }

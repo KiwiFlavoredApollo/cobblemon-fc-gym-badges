b9708977a3a1354ccf2afb12e62acae86e448020
KiwiFlavoredApollo <fractalbarista@gmail.com>
2025-06-01 14:43:09 +0900

    Added item models and textures for Locked Gym Badges and Gym Badge Unlocker

--- src/main/java/kiwiapollo/fcgymbadges/FCGymBadges.java
+++ src/main/java/kiwiapollo/fcgymbadges/FCGymBadges.java
@@ -4,6 +4,7 @@ import com.mojang.logging.LogUtils;
 import kiwiapollo.fcgymbadges.item.LockedGymBadgeItem;
 import kiwiapollo.fcgymbadges.item.GymBadgeItemGroup;
 import kiwiapollo.fcgymbadges.item.GymBadgeItem;
+import kiwiapollo.fcgymbadges.item.MiscItem;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.registry.Registries;
@@ -21,7 +22,8 @@ public class FCGymBadges implements ModInitializer {
         addItemGroup();
 
         addGymBadgeItems();
-        addGymBadgeCrafterItems();
+        addLockedGymBadgeItems();
+        addMiscItems();
     }
 
     private void addItemGroup() {
@@ -40,13 +42,25 @@ public class FCGymBadges implements ModInitializer {
         });
     }
 
-    private void addGymBadgeCrafterItems() {
+    private void addLockedGymBadgeItems() {
         Arrays.stream(LockedGymBadgeItem.values()).forEach(item -> {
             Registry.register(Registries.ITEM, item.getIdentifier(), item.getItem());
         });
 
         ItemGroupEvents.modifyEntriesEvent(GymBadgeItemGroup.GYM_BADGE_ITEM_GROUP_KEY).register(group -> {
             Arrays.stream(LockedGymBadgeItem.values()).forEach(item -> {
+                group.add(item.getItem());
+            });
+        });
+    }
+
+    private void addMiscItems() {
+        Arrays.stream(MiscItem.values()).forEach(item -> {
+            Registry.register(Registries.ITEM, item.getIdentifier(), item.getItem());
+        });
+
+        ItemGroupEvents.modifyEntriesEvent(GymBadgeItemGroup.GYM_BADGE_ITEM_GROUP_KEY).register(group -> {
+            Arrays.stream(MiscItem.values()).forEach(item -> {
                 group.add(item.getItem());
             });
         });
